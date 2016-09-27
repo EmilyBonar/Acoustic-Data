c6c3605cd7ac9a35310ae3535945fb46807b17c6
michaelbonar <michaeldbonar@gmail.com>
2016-09-27 15:12:43 -0500

    Clean up comments
    Removed large chunks of commented code

--- swisscode_commented.m
+++ swisscode_commented.m
@@ -39,8 +39,6 @@ f=[1500:2:2500]; % frequencies to evaluate
 delay = 2; %number of frequencies to chop off the front
 NT=200; % number of periods per frequency point
 A=.5; %amplitude in V
-c=343; %speed of sound
-k=2*pi*f/c;
 
 %% -- Adding output and input channels --
 
@@ -97,7 +95,6 @@ t=t-t(1); %deletes first all other points in t besides initial ref point then sh
 data(1:n0-1,:)=[]; %clears all the voltage data to align with t0
 
 n0b=find(outputSignalcos>.5,1,'first'); %sets the ref signal to start at .5 too
-%not sure how this shift compares to the actual signal shift
 outputSignalcos(1:n0b-1)=[]; %seems to be unused
 
 %  figure()
@@ -148,20 +145,7 @@ for n=1:length(f);
     phaseoff(n,2) = (uPh(n,2) - uPh(n,1)) - (mPh(n,2) - mPh(n,1));
     phaseoff(n,3) = (uPh(n,3) - uPh(n,1)) - (mPh(n,3) - mPh(n,1));
     phaseoff(n,4) = (uPh(n,4) - uPh(n,1)) - (mPh(n,4) - mPh(n,1));
-
-%     phaseoff(n,1) = (uPh(n,1) 
-%     phaseoff(n,1) = phaseoff(n,1) - uPh(n,1)) 
-%     phaseoff(n,1) = phaseoff(n,1) - (mPh(n,1) - mPh(n,1))
-%     
-%     phaseoff(n,2) = (uPh(n,2) 
-%     phaseoff(n,2) = phaseoff(n,2) - uPh(n,1))
-%     phaseoff(n,2) = phaseoff(n,2) - (mPh(n,2) - mPh(n,1));
-%      
-%     phaseoff(n,3) = (uPh(n,3) - uPh(n,1)) - (mPh(n,3) - mPh(n,1));
-%     phaseoff(n,4) = (uPh(n,4) - uPh(n,1)) - (mPh(n,4) - mPh(n,1));
 end
-% size(Ph)
-% size(mph)
 
 %phaseoff=unwrap(Ph)-mph %how much each mic is off at each freq in radians
 figure
@@ -181,11 +165,6 @@ p11=Am(:,2)/c2/m2*m1.*exp(j*Ph(:,2));
 p21=Am(:,3)/c3/m3*m1.*exp(j*Ph(:,3));
 p22=Am(:,4)/c4/m4*m1.*exp(j*Ph(:,4));
 
-% p12=Am(:,1)/c1.*exp(j*Ph(:,1));
-% p11=Am(:,2)/c2.*exp(j*Ph(:,2));
-% p21=Am(:,3)/c3.*exp(j*Ph(:,3));
-% p22=Am(:,4)/c4.*exp(j*Ph(:,4));
-
 % -- Transfer functions --
 H121=p12./p11;
 H221=p22./p21;
@@ -197,12 +176,6 @@ c0=344;
 % -- Wavenumber --
 k=2*pi*f'/c0;
 
-% -- Distances --
-x1 = 0.0;
-x2 = x1+.0254;
-x3 = x2+10.5*.0254;
-x4 = x3+.0254;
-
 % -- Reflection and transmission coefficients -- 
 R1 = (H121.*exp(j*k*x2) - exp(j*k*x1)) ./ (exp(-j*k*x1) - H121.*exp(-j*k*x2));
 R2 = (H221.*exp(-j*k*x3) - exp(-j*k*x4)) ./ (exp(j*k*x4) - H221.*exp(j*k*x3));
@@ -210,62 +183,6 @@ T12 = H2111 .* (exp(j*k*x2) + R1.*exp(-j*k*x2)) ./ (exp(j*k*x3) + 1./R2.*exp(-j*
 
 T = T12.*(1-R1./R2)./(1-(T12./R2).^2);
 R = (R1 - T12.^2./R2)./(1-(T12./R2).^2);
-
-
-% % -- Mass Altenrante Reflection and transmission coefficients --
-% N=length(data);
-% P1 = data(1:floor(N/2)+1,1)./N;
-% P2 = data(1:floor(N/2)+1,2)./N;
-% P3 = data(1:floor(N/2)+1,3)./N;
-% P4 = data(1:floor(N/2)+1,4)./N;
-% P1 = max(P1)
-% P2 = max(P2)
-% P3 = max(P3);
-% P4 = max(P4);
-% 
-% H121 = P1/P2
-% H221 = P4/P3;
-% H2111 = P3/P2;
-% 
-% R1 = (H121.*exp(j*k*x11) - exp(j*k*x12)) ./ (exp(-j*k*x12) - H121.*exp(-j*k*x11));
-% R2 = (H221.*exp(-j*k*x21) - exp(-j*k*x22)) ./ (exp(j*k*x22) - H221.*exp(j*k*x21));
-% T12 = H2111 .* (exp(j*k*x11) + R1.*exp(-j*k*x11)) ./ (exp(j*k*x21) + 1./R2.*exp(-j*k*x21));
-% 
-% t = T12.*(1-R1./R2)./(1-(T12./R2).^2);
-% r = (R1 - T12.^2./R2)./(1-(T12./R2).^2);
-% 
-% T = abs(t).^2;
-% R = abs(r).^2;
-% 
-% disp(['done!'])
-% figure
-% hold on
-% plot(f, abs(T).^2)                  %transmission
-% plot(f, abs(R).^2,'r')              %reflection
-% plot(f, abs(R).^2+abs(T).^2,'g')    %absorbtion
-% grid on
-% ylim([-.5 1.5])
-% legend('T','R','R+T')
-% 
-% 
-% S11 = P1.*conj(P1);
-% S12 = P2.*conj(P1);
-% S21 = P1.*conj(P2);
-% S22 = P2.*conj(P2);
-% 
-% H12 = S12./S11;
-% %H12 = H12./Hc;
-% 
-% HI = exp(-i*k*s);
-% HR = exp(i*k*s);
-% 
-% PrPrC = (1./H12+conj(H12)-conj(H12)./H12.*HI-HR)./(((HR-HI).*conj((HR-HI)))./S12);
-% PiPiC = (1./H12+conj(H12)-conj(H12)./H12.*HR-HI)./(((HR-HI).*conj((HR-HI)))./S12);
-% PtPtC = P3.*conj(P3);
-% 
-% R = PrPrC./PiPiC;
-% T = PtPtC./PiPiC;
-% 
 
 disp(['done!'])
 figure
